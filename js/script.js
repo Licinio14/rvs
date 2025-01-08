a17c72fe9a3cc28698076d2495fc7e8ee6a9998f
gitumbelino <filipe_umbelino@proton.me>
2025-01-08 14:14:43 +0000

    bd

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
-
+	//categorias
+		// women:
+		// 1 - sweaters
+		// 2 - t-shirts
+		// 3 - pants
+		// 4 - shoes
+		
+		// men:
+		// 5 - sweaters
+		// 6 - t-shirts
+		// 7 - pants
+		// 8 - shoes
+		
+		// 9 - promos mulher
+		// 10 - promos homem
+		
+		// 11 - destaques mulher
+		// 12 - destaques homem
+		
+		// 13 - coleções mulher
+		// 14 - coleções homem
+		
+		// 15 - tudo mulher
+		// 16 - tudo homem
+	
 
 function executar(){
 	let produtos = BDquery(1);
