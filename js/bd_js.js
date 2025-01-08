a3ef716dde0710afa573b71f2b9e145bef5647d0
gitumbelino <filipe_umbelino@proton.me>
2025-01-08 11:35:27 +0000

    novo update

--- js/bd_js.js
+++ js/bd_js.js
@@ -1,4 +1,71 @@
-function teste(){
-    const testearray = [1,2,3,4]
-    return testearray;
+function BDprodutos(){
+
+    const produtos = [
+        { id: 1, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 2, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 3, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 4, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 5, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 6, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 7, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 8, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 9, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 10, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 11, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 12, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 13, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 14, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 15, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 16, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 17, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 18, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 19, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 20, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 21, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 22, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 23, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 24, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 25, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 26, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 27, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 28, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 29, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 30, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 31, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 32, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 33, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 34, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 35, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 36, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 37, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 38, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 39, nome: "", preco: 0, descricao: "...", promo: 0, src: "" },
+        { id: 40, nome: "", preco: 0, descricao: "...", promo: 0, src: "" }
+      ];
+
+    return pessoas;
+
+}
+
+function BDcategorias(id){
+
+    //separação de categorias
+    switch (id){
+        //men
+        case 1:
+
+            
+        break;
+        case 2:
+
+        break;
+        case 2:
+
+        break;
+        case 2:
+
+        break;
+
+        //women
+    }
 }
