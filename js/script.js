5cf40066a2918864130bce396b45da986bc50d57
gitumbelino <filipe_umbelino@proton.me>
2025-01-08 16:53:29 +0000

    store mulheres

--- js/script.js
+++ js/script.js
@@ -23,9 +23,75 @@
 		// 15 - tudo mulher
 		// 16 - tudo homem
 	
+//variaveis
+const divMulheres = document.getElementById('divmulheres')
+
+
 
 function Filtrar(){
-	let produtos = BDprodutos();
-	let categoria = BDcategorias(8);
-	console.log(categoria)
+	
+
+}
+
+function PreencherMulheres(){
+	let temp
+	for(let i = 0; i < 20; i++){
+		// card
+		var card = document.createElement('div')  
+        card.className = `card cardstorecss`
+		card.id = `card${i}`
+        divMulheres.appendChild(card)
+		// imagem
+		var card = document.createElement('img')  
+        card.className = `card-img-top`
+		card.src = produtos[i].src
+		temp = document.getElementById(`card${i}`);
+        temp.appendChild(card)
+		// div body
+		var card = document.createElement('div')  
+        card.className = `card-body`
+		card.id = `card-body${i}`
+		temp = document.getElementById(`card${i}`);
+        temp.appendChild(card)
+		// h5
+		var card = document.createElement('h5')  
+        card.className = `card-title`
+		card.textContent = produtos[i].nome
+		temp = document.getElementById(`card-body${i}`);
+        temp.appendChild(card)
+		// descrição
+		var card = document.createElement('p')  
+        card.className = `card-text`
+		card.textContent = produtos[i].descricao
+		temp = document.getElementById(`card-body${i}`);
+        temp.appendChild(card)
+		// div footer
+		var card = document.createElement('div')  
+        card.className = `card-footer`
+		card.id = `card-footer${i}`
+		temp = document.getElementById(`card${i}`);
+        temp.appendChild(card)
+		// div footer
+		var card = document.createElement('small')  
+        card.className = `text-body-secondary`
+		card.textContent = "teste"
+		temp = document.getElementById(`card${i}`);
+        temp.appendChild(card)
+		
+
+    }
+}
+
+function PreencherHomens(){
+
+}
+
+
+//chamar on load
+window.onload = function() {
+	produtos = BDprodutos()
+	categoriaw = BDcategorias(15)
+	categoriah = BDcategorias(16)
+    PreencherMulheres()
+	PreencherHomens()
 }
